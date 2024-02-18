e784dfd08b86669f7246672725566d2ce5eba835
slenane <slenane1@gmail.com>
2024-02-18 11:38:54 +0100

    connectSrc

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(
       connectSrc: [
         "'self'",
         "https://region1.google-analytics.com",
-        "https://roadmapr.dev",
+        config.ENVIRONMENT.apiUrl,
       ],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: [
